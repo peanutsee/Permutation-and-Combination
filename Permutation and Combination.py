378cd3ac8e92e30ee274574ccc120ddaac6d90a5
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-09-09 09:52:35 +0800

    Add files via upload

---
+++ Permutation and Combination.py
@@ -0,0 +1,21 @@
+from math import factorial
+
+
+class PnC:
+    def __init__(self):
+        pass
+
+    def combination(self, n, r):
+        return factorial(n)/(factorial(r) * factorial(n - r))
+
+    def permutation(self, n, r):
+        return self.combination(n, r) * factorial(r)
+
+    def circular_permutation(self, n):
+        return factorial(n - 1)
+
+
+
+
+
+
